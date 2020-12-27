93ce941d1a319314508a51719dbb301728d1089f
Carl Friess <carl.friess@me.com>
2020-12-27 15:49:25 +0100

    Add a script for programming the target

---
+++ tools/flash.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit script if any step fails
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <hex-file>"
+    exit 1
+fi
+
+# This script relies on the external nRF command line tools repository downloaded by Bazel.
+# If the tools repository is missing, build an nRF5_binary target first.
+BAZEL_OUTPUT_BASE=$(bazel info output_base)
+
+# Select tools repository based on host OS
+case "$(uname -s)" in
+    Linux*)     TOOLS="$BAZEL_OUTPUT_BASE/external/nRF_tools_linux";;
+    Darwin*)    TOOLS="$BAZEL_OUTPUT_BASE/external/nRF_tools_macOS";;
+    *)          echo "Error: unsupported host platform"; exit 1
+esac
+
+NRFJPROG="$TOOLS/nrfjprog/nrfjprog"
+
+if [ ! -f "$NRFJPROG" ]; then
+    echo "Error: nrfjprog is missing"
+    exit 1
+fi
+
+$NRFJPROG --program $1 --chiperase --reset
