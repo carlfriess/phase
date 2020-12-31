66107b582c4b6f2db0d9d0df305f7be7958c3d82
Carl Friess <carl.friess@me.com>
2020-12-31 19:05:29 +0100

    Move BLE related functions to separate file

---
+++ watch/bluetooth.h
@@ -0,0 +1,34 @@
+//
+// Created by Carl Friess on 31/12/2020.
+//
+
+#ifndef PHASE_BLUETOOTH_H
+#define PHASE_BLUETOOTH_H
+
+#include <stdbool.h>
+
+void advertising_start(bool erase_bonds);
+
+void timers_init(void);
+
+void gap_params_init(void);
+
+void gatt_init(void);
+
+void services_init(void);
+
+void ble_stack_init(void);
+
+void scheduler_init(void);
+
+void conn_params_init(void);
+
+void buttons_leds_init(bool *p_erase_bonds);
+
+void peer_manager_init(void);
+
+void advertising_init(void);
+
+void db_discovery_init(void);
+
+#endif //PHASE_BLUETOOTH_H
