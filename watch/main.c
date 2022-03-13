c18ab6af24ac46a99391766eafdc57ee7aa43266
Carl Friess <carl.friess@me.com>
2022-03-13 18:44:00 +0100

    Play haptic feedback for notifications

--- watch/main.c
+++ watch/main.c
@@ -61,6 +61,7 @@ void bluetooth_time_handler(time_t time) {
 
 void bluetooth_notification_add(char *appid, char *title, char *msg) {
     ui_add_notification(appid, title, msg);
+    haptics_play_effect(&twi_manager, 54);
 }
 
 static void time_sync_handler(void *ctx) {
